e5841c19998ad039d7a2a00b178ba7ee2f674456
Mohammad <75981970+mohammad0124@users.noreply.github.com>
2022-09-25 23:16:33 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import os, platform, time, sys
+import ok
+import cowsay
+from colorama import Fore
+
+name = platform.uname()
+
+
+def Ok():
+    os.system("clear" or "cls")
+
+    a = int(input("Hello enter the platform:\n1)System\n2)node\n3)release\n4)version\n5)machine\n6)developer"
+                  "\n7)exit "
+                  "\nchoose:"))
+    if a == 1:
+        name.system == "Linux"
+        print(cowsay.tux(Fore.GREEN + "Hello your system is Linux"))
+
+        print(Fore.LIGHTBLUE_EX + f"Hello your system is {name.system}")
+    elif a == 1:
+        name.system == "Windows"
+
+        print(cowsay.dragon(Fore.RED + "Hello your System is Windows"))
+
+        print(Fore.LIGHTBLUE_EX + f"Hello your system is {name.system}")
+
+    elif a == 2:
+        print(Fore.LIGHTBLUE_EX + f"Hello your node is {name.node}")
+
+    elif a == 3:
+        print(Fore.LIGHTBLUE_EX + f"Hello your release is {name.release}")
+
+    elif a == 4:
+        print(Fore.LIGHTBLUE_EX + f"Hello your version is {name.version}")
+
+    elif a == 5:
+        print(Fore.LIGHTBLUE_EX + f"Hello your TypeSystem is {name.machine}")
+
+    elif a == 6:
+        print(Fore.LIGHTBLUE_EX + "Hello my name is Mohammad :)")
+
+    elif a == 7:
+        sys.exit("Hello exit")
+
+    else:
+        print("try agin")
+
+        time.sleep(2)
+        return Ok()
+
+
+Ok()
---
+++ ok.py
@@ -0,0 +1,7 @@
+import os
+from time import sleep
+a = os.system("pip install pyfiglet" or "pip3 install pyfiglet")
+sleep(2)
+b = os.system("pip install cowsay" or "pip3 install cowsay")
+sleep(2)
+c = os.system("pip install colorama" or "pip3 install colorama")
